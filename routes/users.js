a15695d0d06c67019e457273b6ede2152d329264
Mayank Jain <jainmayank2509@gmail.com>
2023-07-02 02:23:42 +0530

    added create user route

--- routes/users.js
+++ routes/users.js
@@ -23,4 +23,14 @@ router.get('/:id', async (req, res)=>{
 }
 )
 
+router.post('/',async (req, res)=>{
+            const user = await User.create(req.body)
+            if(!user){
+                return res.status(400).send({error: 'User could not be created'})
+            }
+            else{
+                res.status(200).send(user)
+            }
+})
+
 module.exports = router
